3c1d0e71df2a1c0a73faa040e1c30f0975eb517f
hai2k1 <trihai306201@gmail.com>
2022-10-28 18:10:00 +0700

    update

--- src/Cms/Providers/CmsServiceProvider.php
+++ src/Cms/Providers/CmsServiceProvider.php
@@ -43,6 +43,6 @@ class CmsServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../../../theme' => public_path('themes'),
-        ], 'duxphp/hairavel-cms');
+        ], 'haibase/hairavel-cms');
     }
 }
