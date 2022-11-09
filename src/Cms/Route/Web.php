fdcc620aeb6755851df940f5634ab0553802df26
hai2k1 <trihai306201@gmail.com>
2022-11-09 13:47:33 +0700

    update

--- src/Cms/Route/Web.php
+++ src/Cms/Route/Web.php
@@ -2,26 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-// The default route of the website home page (can be customized to override)
-Route::get('/', [\Modules\Cms\Web\Index::class, 'index'])->middleware('web')->name('web.index');
-
-Route::group([
-    'app' => 'content management'
-], function () {
-    Route::group([
-        'group' => 'custom page'
-    ], function () {
-        Route::get('page/{id}', ['uses' => 'Modules\Cms\Web\Pages@index', 'desc' => 'page'])->name('web.pages' );
-    });
-
-
-    Route::group([
-        'prefix' => 'form',
-        'group' => 'custom form'
-    ], function () {
-        Route::get('list/{id}', ['uses' => 'Modules\Cms\Web\Form@index', 'desc' => 'list'])->name('web.form.list');
-        Route::get('info/{id}', ['uses' => 'Modules\Cms\Web\Form@info', 'desc' => 'details'])->name('web.form.info');
-        Route::post('push/{id}', ['uses' => 'Modules\Cms\Web\Form@push', 'desc' => 'post'])->name('web.form.push');
-    });
-
-});
+//// The default route of the website home page (can be customized to override)
+//Route::get('/', [\Modules\Cms\Web\Index::class, 'index'])->middleware('web')->name('web.index');
+//
+//Route::group([
+//    'app' => 'content management'
+//], function () {
+//    Route::group([
+//        'group' => 'custom page'
+//    ], function () {
+//        Route::get('page/{id}', ['uses' => 'Modules\Cms\Web\Pages@index', 'desc' => 'page'])->name('web.pages' );
+//    });
+//
+//
+//    Route::group([
+//        'prefix' => 'form',
+//        'group' => 'custom form'
+//    ], function () {
+//        Route::get('list/{id}', ['uses' => 'Modules\Cms\Web\Form@index', 'desc' => 'list'])->name('web.form.list');
+//        Route::get('info/{id}', ['uses' => 'Modules\Cms\Web\Form@info', 'desc' => 'details'])->name('web.form.info');
+//        Route::post('push/{id}', ['uses' => 'Modules\Cms\Web\Form@push', 'desc' => 'post'])->name('web.form.push');
+//    });
+//
+//});
